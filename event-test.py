05f2dc7a5f59138bd03e8d590398631565d05f72
seweryn123 <01114779@pw.edu.pl>
2025-06-09 19:35:09 +0200

    skill on - test

--- event-test.py
+++ event-test.py
@@ -1,21 +1,30 @@
 from asyncio import run, sleep
 from concurrent.futures import ThreadPoolExecutor
-from xbot.events.keyboard import press_key
+from xbot.events.keyboard import press_key, send_key_string
 from xbot.events.mouse import MOUSEEVENTF_RIGHTDOWN, MOUSEEVENTF_RIGHTUP, mouseClick, mouseMoveSmooth, mousePos
 
 
 async def test():
     executor = ThreadPoolExecutor()
-    
-    await sleep(5)
-    await press_key(executor, 'f5')
 
-    await sleep(1)
-    await press_key(executor, 'f6')
+
+    # Sekwencja włączania skilli
+    #Zsiądź z konia ctr+g
+    await sleep(5)
+    await press_key(executor, 'ctrl')
+    await press_key(executor, 'g')
+
+    #Skill f1
+    await sleep(5)
+    await press_key(executor, 'f1')
+
+    #Skill f2
+    await sleep(5)
+    await press_key(executor, 'f2')
+
+    print(mousePos())
 
     # await send_key_string(executor, 'eELoO')
-    # print(mousePos())
-
     # mouseClick(MOUSEEVENTF_RIGHTDOWN)
     # await sleep(1)
     # # mouseMove(100, 100)
